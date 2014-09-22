f9859261006e320cb93db9af002e27e01068d044
Gilmar Pupo <g@g1mr.com>
2014-09-22 15:22:43 -0300

    Novos traits

---
+++ src/Gpupo/CommonSdk/Traits/LoggerTrait.php
@@ -0,0 +1,20 @@
+<?php
+namespace Gpupo\CommonSdk\Traits;
+
+trait LoggerTrait
+{
+    use \Psr\Log\LoggerTrait;
+    use Psr\Log\LoggerAwareTrait;
+    
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+    
+    public function log($level, $message, array $context = array())
+    {
+        if ($this->getLogger()) {
+            return $this->getLogger()->log($level, $message, $context);
+        }
+    }
+}
