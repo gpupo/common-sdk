626279bc1d8507719e540e8d8b7a6f14028490ba
Gilmar Pupo <g@g1mr.com>
2014-09-24 13:58:15 -0300

    Map

--- src/Gpupo/CommonSdk/Entity/ManagerInterface.php
+++ src/Gpupo/CommonSdk/Entity/ManagerInterface.php
@@ -4,7 +4,4 @@ namespace Gpupo\CommonSdk\Entity;
 
 interface ManagerInterface
 {
-    public function save(EntityInterface $entity);
-    public function findById($id);
-    public function fetch($offset = 1, $limit = 50);
 }
