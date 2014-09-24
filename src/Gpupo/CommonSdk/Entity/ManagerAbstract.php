b5f72d4d25a693017ecb59e720a6e6d6f408f32e
Gilmar Pupo <g@g1mr.com>
2014-09-24 13:58:15 -0300

    Map

--- src/Gpupo/CommonSdk/Entity/ManagerAbstract.php
+++ src/Gpupo/CommonSdk/Entity/ManagerAbstract.php
@@ -4,10 +4,55 @@ namespace Gpupo\CommonSdk\Entity;
 
 use Gpupo\CommonSdk\ClientInterface;
 use Gpupo\CommonSdk\Exception\ManagerException;
+use Gpupo\CommonSdk\Map;
 
 abstract class ManagerAbstract
 {
     protected $client;
+    
+    protected $maps;
+    
+    public function save(EntityInterface $entity)
+    {
+        return $this->execute($this->factoryMap('save'), $entity->toJson());
+    }
+
+    public function findById($itemId)
+    {       
+        $response =  $this->perform($this->factoryMap('findById',
+            ['itemId' => $itemId]));
+        $product = new Product($response->getData()->toArray());
+
+        return $product;
+    }
+
+    public function fetch($offset = 1, $limit = 50)
+    {
+        $response =  $this->perform($this->factoryMap('fetch',
+            ['offset' => $offset, 'limit' => $limit]));
+
+        $product = new Product($response->getData()->toArray());
+
+        return $product;
+    }
+
+    public function factoryMap($operation, array $parameters = null)
+    {
+        if (!is_array($this->maps)) {
+            throw new ManagerException('Maps missed!');
+        }
+        
+        if (!array_key_exists($operation, $this->maps)) {
+            throw new ManagerException('Map not found');
+        }
+        
+        $data = $this->maps[$operation];
+        if (!is_array($data)) {
+            throw new ManagerException('Map MUST be array');
+        }
+        
+        return new Map($data, $parameters);        
+    }
 
     public function __construct(ClientInterface $client)
     {
@@ -25,20 +70,21 @@ abstract class ManagerAbstract
             . $exception->getMessage(), $exception->getCode(), $exception);
     }
 
-    protected function perform($method, $resource, $body = null)
+    protected function perform(Map $map, $body = null)
     {
-        $methodName = strtolower($method);
+        $methodName = strtolower($map->getMethod());
 
         try {
-            return $this->getClient()->$methodName($resource, $body);
+            return $this->getClient()->$methodName($map->getResource(), $body);
         } catch (\Exception $exception) {
-            throw $this->exceptionHandler($exception, $method, $resource);
+            throw $this->exceptionHandler($exception, $map->getMethod(),
+                $map->getResource());
         }
     }
 
-    protected function execute($method, $resource, $body = null)
+    protected function execute(Map $map, $body = null)
     {
-        $this->perform($method, $resource, $body);
+        $this->perform($map, $body);
 
         return true;
     }
---
+++ src/Gpupo/CommonSdk/Map.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Gpupo\CommonSdk;
+
+use Gpupo\CommonSdk\Entity\Collection;
+
+class Map extends Collection
+{
+    public function __construct(array $elements = array(), array $parameters = null)
+    {
+        $data = [
+            'method'        => $elements[0],
+            'resource'      => $elements[1],
+            'parameters'    => $parameters,
+        ];
+        
+        parent::__construct($data);
+    }
+    
+    public function getResource()
+    {
+        $route = $this->get('resource');
+        $parameters = $this->getParameters();
+        if($parameters) {
+            foreach ($parameters as $key => $value) {
+                $route = str_replace("{" . $key . "}", $value, $route);
+            }
+        }
+        
+        return $route;
+    }
+}
