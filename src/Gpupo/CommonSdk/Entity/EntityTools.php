dc23fa61d80b94f8c28b956d6f6070253356b2c1
Gilmar Pupo <g@g1mr.com>
2014-10-02 10:27:25 -0300

    EntityTools

---
+++ src/Gpupo/CommonSdk/Entity/EntityTools.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Gpupo\CommonSdk\Entity;
+
+class EntityTools
+{
+    public static function getInitValue($data, $key, $default = '')
+    {
+        if (is_array($data) && array_key_exists($key, $data)) {
+            $fill = $data[$key];
+            if (is_array($default) && !is_array($fill)) {
+                $fill = [$key => $fill];
+            }
+        }
+
+        return (isset($fill)) ? $fill : $default;
+    }
+
+    public static function normalize($data, $type)
+    {
+        switch ($type) {
+            case 'integer':
+                return (int) $data;
+            case 'boolean':
+                return (boolean) $data;
+            default:
+                return $data;
+        }
+    }
+
+    public static function validate($current, $value)
+    {
+        if ($value == 'integer') {
+            if (!empty($current) && intval($current) !== $current) {
+                throw new \InvalidArgumentException($key
+                    . ' should have value of type Integer valid (received '
+                    . $current . ')');
+            }
+        } elseif (!empty($current) && $value == 'number') {
+            if (!is_numeric($current)) {
+                throw new \InvalidArgumentException($key
+                    . ' should have value of type Number valid');
+            }
+        }
+
+        return true;
+    }
+ 
+}
