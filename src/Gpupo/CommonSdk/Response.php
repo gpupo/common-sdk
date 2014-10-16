56be4640b273fa6f4dbef18ddf7c23253cca327e
Gilmar Pupo <g@g1mr.com>
2014-10-16 10:25:51 -0300

    Lidando com requisicoes vazias

--- src/Gpupo/CommonSdk/Response.php
+++ src/Gpupo/CommonSdk/Response.php
@@ -91,7 +91,12 @@ class Response extends Collection
 
     public function getData()
     {
-        return new Collection(json_decode($this->get('responseRaw'), true));
+        $data = json_decode($this->get('responseRaw'));
+        if (!is_array($data)) {
+            $data = [];
+        }
+        
+        return new Collection($data, true);
     }
 
     public function toLog()
