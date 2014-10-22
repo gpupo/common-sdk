6f26452dc68e51b899f2d9d87dd9f6c422a87500
Gilmar Pupo <g@g1mr.com>
2014-10-22 16:14:55 -0200

    Cache

---
+++ src/Gpupo/CommonSdk/Traits/CacheTrait.php
@@ -0,0 +1,16 @@
+<?php
+namespace Gpupo\CommonSdk\Traits;
+
+use Gpupo\Cache\CacheAwareTrait;
+
+trait CacheTrait
+{
+    use CacheAwareTrait;
+
+    public function initCache($cacheItemPool)
+    {
+        if (!empty($cacheItemPool {
+            return $this->setCacheItemPool($logger);
+        }
+    }
+}
