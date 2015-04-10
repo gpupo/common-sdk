1d08bbd030ceb22488ddc5bed020f1c2a6311692
Gilmar Pupo <g@g1mr.com>
2015-04-10 17:05:13 -0300

    Objeto Factory

---
+++ tests/FactoryTestAbstract.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of common-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\Tests\CommonSdk;
+
+abstract class FactoryTestAbstract extends TestCaseAbstract
+{
+    abstract public function getFactory();
+    
+    abstract public function dataProviderObjetos();
+    
+    protected function createObject($factory, $method, $data = null)
+    {
+        return $factory->$method($data);
+    }
+    
+    protected function assertFactoryWorks($objectExpected, $factory, $method, array $data = null)
+    {
+        return $this->assertInstanceOf($objectExpected,
+            $this->createObject($factory, $method, $data));
+    }
+    
+    /**
+     * @dataProvider dataProviderObjetos
+     */
+    public function testCentralizaCriacaoDeObjetos($objectExpected, $name, array $data = null)
+    {
+        $method = 'create'.ucfirst($name);
+        
+        return $this->assertFactoryWorks($objectExpected, $this->getFactory(), $method, $data);
+    }
+    
+    
+}
