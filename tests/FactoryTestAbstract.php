e50cec05dd245bb7bade1346520d1a8f6243f568
Gilmar Pupo <g@g1mr.com>
2015-04-13 09:25:14 -0300

    cs fix

--- tests/FactoryTestAbstract.php
+++ tests/FactoryTestAbstract.php
@@ -14,29 +14,27 @@ namespace Gpupo\Tests\CommonSdk;
 abstract class FactoryTestAbstract extends TestCaseAbstract
 {
     abstract public function getFactory();
-    
+
     abstract public function dataProviderObjetos();
-    
+
     protected function createObject($factory, $method, $data = null)
     {
         return $factory->$method($data);
     }
-    
+
     protected function assertFactoryWorks($objectExpected, $factory, $method, array $data = null)
     {
         return $this->assertInstanceOf($objectExpected,
             $this->createObject($factory, $method, $data));
     }
-    
+
     /**
      * @dataProvider dataProviderObjetos
      */
     public function testCentralizaCriacaoDeObjetos($objectExpected, $name, array $data = null)
     {
         $method = 'create'.ucfirst($name);
-        
+
         return $this->assertFactoryWorks($objectExpected, $this->getFactory(), $method, $data);
     }
-    
-    
 }
