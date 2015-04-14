9e40103ebe9f2e8f5c99c823e3f16e29ce948bde
Gilmar Pupo <g@g1mr.com>
2015-04-14 11:13:02 -0300

    Client/Oauth2/Provider

---
+++ src/Client/Client.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of common-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\CommonSdk\Client;
+
+final class Client extends ClientAbstract
+{
+    public function getDefaultOptions()
+    {
+        return [];
+    }
+}
