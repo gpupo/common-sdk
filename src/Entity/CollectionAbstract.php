86e91ede1d3ad25fb283a1f467fb13a702d4eb6f
Gilmar Pupo <g@g1mr.com>
2015-05-15 17:44:03 -0300

    CollectionAbstract

---
+++ src/Entity/CollectionAbstract.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of gpupo/common-sdk
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\CommonSdk\Entity;
+
+use Gpupo\Common\Entity\CollectionAbstract as Common;
+
+abstract class CollectionAbstract extends Common
+{
+    abstract public function factoryElement($data);
+
+    public function __construct(array $elements = [])
+    {
+        $list = [];
+
+        foreach ($elements as $data) {
+            $list[] = $this->factoryElement($data);
+        }
+
+        parent::__construct($list);
+    }
+}
