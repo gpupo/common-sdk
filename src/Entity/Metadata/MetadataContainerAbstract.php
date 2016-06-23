65c86e9dcddfde35d5951089de7be3150847886b
Gilmar Pupo <g@g1mr.com>
2016-06-23 10:35:06 -0300

    header

--- src/Entity/Metadata/MetadataContainerAbstract.php
+++ src/Entity/Metadata/MetadataContainerAbstract.php
@@ -1,15 +1,11 @@
 <?php
 
 /*
- * This file is part of gpupo/cnova-sdk
- *
- * (c) Gilmar Pupo <g@g1mr.com>
- *
+ * This file is part of gpupo/common-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * For more information, see
- * <http://www.g1mr.com/cnova-sdk/>.
+ * For more information, see <http://www.g1mr.com/common-sdk/>.
  */
 namespace Gpupo\CommonSdk\Entity\Metadata;
 
--- src/Entity/Schema/Tools.php
+++ src/Entity/Schema/Tools.php
@@ -2,14 +2,10 @@
 
 /*
  * This file is part of gpupo/common-sdk
- *
- * (c) Gilmar Pupo <g@g1mr.com>
- *
+ * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * For more information, see
- * <http://www.g1mr.com/common-sdk/>.
+ * For more information, see <http://www.g1mr.com/common-sdk/>.
  */
 namespace Gpupo\CommonSdk\Entity\Schema;
 
