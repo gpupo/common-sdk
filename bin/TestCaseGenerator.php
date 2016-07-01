e7d1ca3e8b7c9037f86409a8a62e767013b4871d
Gilmar Pupo <g@g1mr.com>
2016-07-01 14:09:41 -0300

    TestCaseGenerator

---
+++ bin/TestCaseGenerator.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+use Gpupo\Tests\CommonSdk\Traits\EntityTrait;
+use Gpupo\Tests\NetshoesSdk\TestCaseAbstract;
+
+class TestCaseGenerator extends TestCaseAbstract
+{
+    use EntityTrait;
+
+    public static function setUpBeforeClass()
+    {
+        global $argv;
+        $className = $argv[3];
+
+        if (empty($className)) {
+            throw new \Exception('Fully Qualified Object Name missed!', 1);
+        }
+
+        static::setFullyQualifiedObject($className);
+        parent::setUpBeforeClass();
+    }
+}
