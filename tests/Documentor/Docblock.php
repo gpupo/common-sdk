97a9b2de537e556fe26a13ce0458a76849625399
Gilmar Pupo <g@g1mr.com>
2016-07-01 15:36:50 -0300

    bin/generate-testcases.sh

--- tests/Documentor/Docblock.php
+++ tests/Documentor/Docblock.php
@@ -85,16 +85,16 @@ class Docblock
         $data['classShortName'] = end($array);
         $data['objectShortName'] = lcFirst($data['classShortName']);
         array_pop($array);
-        $data['classNamespace'] = implode('\\', $array);
+        $data['classNamespace'] = implode("\\", $array);
         $data['mainNamespace'] = $array[1];
-        $array[2] = 'Tests\\'.$array[2];
-        $data['testNamespace'] = implode('\\', $array);
+        $array[0]= $array[0] . '\\Tests';
+        $data['testNamespace'] = implode("\\", $array);
         $dest = $this->getResourcesDestinationPath("testCase_{$data['class']}.php");
         $data['asserts'] = $this->renderAsserts($data);
         $data['expected'] = $this->renderExpected($data);
 
         if ($dest) {
-            echo 'Test Case file generated: '.$dest."\n";
+            echo 'Test Case file generated: ' . $dest. "\n";
             file_put_contents($dest, $this->render($data, 'testCase'));
         }
     }
@@ -105,7 +105,7 @@ class Docblock
 
         if ($dest) {
             file_put_contents($dest, $json);
-            echo 'Json file generated: '.$dest."\n";
+            echo 'Json file generated: ' . $dest. "\n";
         }
     }
 
