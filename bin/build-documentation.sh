3292a50e82aba846b46bd3718c790a2d3960717a
Gilmar Pupo <g@g1mr.com>
2016-07-06 16:42:54 -0300

    wiki-build

--- bin/build-documentation.sh
+++ bin/build-documentation.sh
@@ -2,6 +2,15 @@
 # @Date:   2016-06-24T09:45:16-03:00
 # @Modified at 2016-06-24T09:45:37-03:00
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+fi
+
+PROJECT_NAME=$1;
+
+echo "Build documentation for [${PROJECT_NAME}] project";
+
 cat build/logs/testdox.txt | grep -vi php |  sed "s/.*\[/-&/" | \
 sed 's/.*Gpupo.*/&\'$'\n/g' | sed 's/.*Gpupo.*/&\'$'\n/g' |\
 sed 's/Gpupo\\Tests\\/### /g' > Resources/doc/testdox.md;
@@ -20,3 +29,24 @@ do
   cat Resources/doc/${name}.md >> README.md;
   printf "\n"  >>  README.md;
 done
+
+echo "Build wiki for [${PROJECT_NAME}] project";
+
+if [ ! -d "var/wiki" ]; then
+  git clone --depth=1  git@github.com:${PROJECT_NAME}.wiki.git var/wiki
+fi
+
+cd var/wiki
+git checkout --orphan auto
+rm -f ./*.md;
+cp ../../Resources/doc/* .
+cp _Sidebar.md Home.md;
+git add *.md
+git commit -am 'auto'
+git push -f origin auto:master
+git fetch -f origin master:master
+git checkout master
+git branch -D auto
+
+echo "done!";
+exit 0;
