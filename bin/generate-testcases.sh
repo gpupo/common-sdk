4504bdc7102e01528f6479565b6c633111f274da
Gilmar Pupo <g@g1mr.com>
2016-07-15 17:42:22 -0300

    rm bin/generate-testcases.sh

--- bin/generate-testcases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# @Date:   2016-07-01T15:21:50-03:00
-# @Modified at 2016-07-05T11:04:22-03:00
-# {release_id}
-
-mkdir -p var/autodoc;
-eval "$(php vendor/gpupo/common-sdk/bin/ClassesFinder.php)"
