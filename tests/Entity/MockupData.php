3200437159480209cb1f835e96fc9f8cb3c09f8b
Gilmar Pupo <g@g1mr.com>
2016-08-01 14:45:31 -0300

    MockupData

---
+++ tests/Entity/MockupData.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of gpupo/common-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\Tests\CommonSdk\Entity;
+
+/**
+ * @covers \Gpupo\CommonSdk\Entity\EntityAbstract
+ */
+class MockupData
+{
+    public static function create($className)
+    {
+        $object = new $className();
+
+        $data = [];
+
+        foreach ($object->getSchema() as $k => $v) {
+            if ('object' === $v || 'array' === $v) {
+                $x = [];
+            } elseif ('number' === $v) {
+                $x = rand(99, 99999) / rand(2, 222);
+            } elseif ('integer' === $v) {
+                $x = rand(99, 99999);
+            } else {
+                $x = uniqid();
+            }
+
+            $data[$k] = $x;
+        }
+
+        return $data;
+    }
+}
