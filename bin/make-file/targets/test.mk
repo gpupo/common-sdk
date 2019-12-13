349dba88326a9465ff518201ff0a2a572eddc3e2
Gilmar Pupo <contact@gpupo.com>
2019-12-13 16:45:13 -0300

    Add travis make target

--- bin/make-file/targets/test.mk
+++ bin/make-file/targets/test.mk
@@ -1,3 +1,8 @@
 ## Run phpunit testcases
 test@phpunit:
 	${VENDOR_BIN}/phpunit --testdox
+
+## Run in travis execution
+test@travis-script: install
+test@travis-script:
+	APP_ENV=test ${VENDOR_BIN}/phpunit --testdox
