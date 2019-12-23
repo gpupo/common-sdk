2ee061842c8226d659494934a9c20a965d64fc58
Gilmar Pupo <contact@gpupo.com>
2019-12-23 15:20:37 -0300

    Test vendor

--- bin/make-file/variables.mk
+++ bin/make-file/variables.mk
@@ -2,6 +2,9 @@ VENDOR_BIN=./vendor/bin
 CORE_FILES=./vendor/gpupo/common/bin/make-file/
 COMMON_SDK_BIN=./vendor/gpupo/common-sdk/bin
 
-include ${CORE_FILES}/variables.mk
-include ${CORE_FILES}/define.mk
-include ${CORE_FILES}/help.mk
+#Common
+ifneq ($(wildcard ${CORE_FILES}/*),)
+	include ${CORE_FILES}/variables.mk
+	include ${CORE_FILES}/define.mk
+	include ${CORE_FILES}/help.mk
+endif
